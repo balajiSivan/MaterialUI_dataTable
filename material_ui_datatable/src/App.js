789d084fd194a1962acff3934989e4f33f3ea143
balaji <balajiibaskar@gmail.com>
2022-02-15 14:25:16 +0530

    final commit

---
+++ material_ui_datatable/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import DataTable from "./components/DataBase";
+
+function App() {
+  return (
+    <div className="App">
+      <DataTable></DataTable>
+    </div>
+  );
+}
+
+export default App;
